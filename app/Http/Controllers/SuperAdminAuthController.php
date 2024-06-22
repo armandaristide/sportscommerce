284099926d4b2ec0f2b06e368cd45c59143bf586
armandaristide <aristidelangia@yahoo.com>
2024-06-22 18:35:51 +0200

    Added teamsports to sportscommerce

---
+++ app/Http/Controllers/SuperAdminAuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+class SuperAdminAuthController extends Controller
+{
+    //
+    public function login():View
+    {
+        return view('auth.superAdminLogin');
+    }
+
+    public function loginSubmit(Request $request){
+
+        $request->validate([
+                'email'=>'required|email',
+                'password'=>'required'
+            ]
+        );
+
+        $input = $request->all();
+        $data = [
+            'email' => $input['email'],
+            'password' => $input['password']
+        ];
+
+
+        // check if the given user exists in db
+        if(Auth::attempt(['email'=> $data['email'], 'password'=> $data['password']])){
+            // check the user role
+            if (Auth::user()->type == 2) {
+                return redirect()->route('superAdminDashboardShow');
+            }else{
+                return redirect()->route('superAdminLogin')->with('error', "You dont have permission to access");
+            }
+
+        }
+        else{
+            return redirect()->route('superAdminLogin')->with('error', "Wrong credentials");
+        }
+
+
+
+    }
+
+    public function superAdminLogout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
