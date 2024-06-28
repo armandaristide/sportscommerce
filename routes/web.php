5c532c18eeb5db5f4a073f9e9fc1f59f84bfe172
armandaristide <aristidelangia@yahoo.com>
2024-06-28 14:03:06 +0200

    fixed web route for gensubcat

--- routes/web.php
+++ routes/web.php
@@ -57,7 +57,7 @@ Route::get('/product', [PagesController::class, 'product'])->name('product');
 Route::get('/', [PagesController::class, 'index'])->name('index');
 Route::get('/product_details_{prod}', [PagesController::class, 'productDetails'])->name('productdets');
 Route::get('/subcategories_{prod}', [PagesController::class, 'subcategory'])->name('subcat');
-Route::get('/{prod}', [PagesController::class, 'generalsub'])->name('generalsub');
+Route::get('/p_{prod}', [PagesController::class, 'generalsub'])->name('generalsub');
 
 /**General user routes **/
 Route::middleware(['auth', 'verified'])->get('/dashboard', [DashboardController::class, 'generalUserDashboard'])->name('dashboard');
