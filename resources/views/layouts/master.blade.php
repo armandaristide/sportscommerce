de6da7149c3a2fb265057ed3d97637b13e4ca8de
armandaristide <aristidelangia@yahoo.com>
2024-07-02 15:37:16 +0200

    carts started

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -175,7 +175,7 @@
                                                     <li class="onhover-dropdown wislist-dropdown">
                                                         <div class="cart-media">
                                                             <a href={{route('cart')}}>  <div class="cart-icon">
-                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart pe-2"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>  <span class="label label-theme rounded-pill">{{count($carts)}}</span>
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart pe-2"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>  <span class="label label-theme rounded-pill">{{count(\App\Models\Cart::all())}}</span>
                                                                 </div></a>
 
                                                         </div>
