d578aab3636fed865b3b2786383579a638d547f5
armandaristide <aristidelangia@yahoo.com>
2024-07-05 12:22:29 +0200

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -217,13 +217,13 @@
                                             <div class="img-wrapper">
                                                 <div class="front">
                                                     <a href="{{route('productdets',$product->id)}}">
-                                                        <img src="https://fadzrinmadu.github.io/hosted-assets/product-detail-page-design-with-image-slider-html-css-and-javascript/shoe_2.jpg"
+                                                        <img src="{{$product->imageone}}"
                                                              class="bg-img blur-up lazyload" alt="">
                                                     </a>
                                                 </div>
                                                 <div class="back">
                                                     <a href="{{route('productdets',$product->id)}}">
-                                                        <img src="https://fadzrinmadu.github.io/hosted-assets/product-detail-page-design-with-image-slider-html-css-and-javascript/shoe_4.jpg"
+                                                        <img src="{{$product->imagetwo}}"
                                                              class="bg-img blur-up lazyload" alt="">
                                                     </a>
                                                 </div>
