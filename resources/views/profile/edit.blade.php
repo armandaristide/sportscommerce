771a08c7702997a249cbd07c7186f5479d38dcd1
SparkyPilot <sherwinalva@gmail.com>
2024-07-18 10:27:33 +0200

    User story 60 and 61. Not working yet though. Still work is pending on it.

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -9,7 +9,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
+                    <p>Name: {{ $user->name }}</p>
+                    <p>Email: {{ $user->email }}</p>
+
+                    <a href="{{ route('profile.edit') }}" class="btn btn-primary">Edit Profile</a>
                 </div>
             </div>
 
